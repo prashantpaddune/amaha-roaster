9efa2549ce28bd84241b8e10b7b8c61f5de47c86
prashantpaddune <padadunep@gmail.com>
2025-05-09 14:10:16 +0530

    feat: match styles and font as figma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div className="flex flex-col items-center justify-center w-full">
-          <h1>Amaha Roaster</h1>
+          <h1>Provider Calendar</h1>
       </div>
   );
 }
